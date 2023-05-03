d336cb18ce97937793321f1d5985d62b7969ac14
unknown <toukirraju@gmail.com>
2023-05-03 23:12:50 +0600

    update projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,6 +182,7 @@ window.addEventListener("scroll", scrollHeader);
 
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
+
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
   else scrollUp.classList.remove("show-scroll");
